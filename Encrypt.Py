c0a5b61f831c1d0ba027146b2db0ba9565efb69b
itsmadelineyo <madison.dorse@gmail.com>
2025-02-15 16:16:26 -0600

    Encrypt.Py

---
+++ Encrypt.Py
@@ -0,0 +1,11 @@
+>>> message = input("Enter a message: ")
+Enter a message: Hello world!
+>>> distanceValue = int(input("Enter distance value: "))
+Enter distance value: 4
+>>> result = ""
+>>> for x in message:
+...     result += chr(ord(x)+distanceValue)
+...
+>>> print(""+result)
+Lipps${svph%
+>>>
